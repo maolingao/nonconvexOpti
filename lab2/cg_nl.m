82d4fdcd987b0ba511af291784e4e2ef20a22877
maolin@oben <maolin.gao@gmail.com>
2015-06-16 19:18:20 +0200

    exact line-search for quad works for linear system, but amijo always delivers near ZERO step size!

--- lab2/cg_nl.m
+++ lab2/cg_nl.m
@@ -10,37 +10,44 @@ if ~exist('aux','var');
     aux.itr = 100; 
     aux.tol = 1e-6;
 end
-keyboard
+% keyboard
 x = x0;
-r = grad(x);                          % initial gradient
-p = -r;        % initial gradient descent 
+r = grad(x);   % initial gradient
+p = -r;
 
+err= [];
 for k = 1 : aux.itr
-
-%         q = aux.A*p; % A*p register
-%         alpha = ((p'*q) + eps)\(r'*r);
-    alpha = armijo(fun,x,r,p);          % step length - armijo
+    
+    % fig - residual
+    err = [err,norm(r)];
+    figure(2), plot(err,'b'), drawnow,hold on, set(gca,'Yscale','log');
+    
+    % main computation
+        % exact line-search for quadratic
+    q = aux.A*p;
+    alpha = ((p'*q) + eps)\(r'*r);
 %     alpha = analyticlineSearch_cg(aux.A,aux.B,aux.C,x,p); % analytical line search
+        % armijo 
+        % #### alpha is always ZERO!! ####
+%     alpha = armijo(fun,x,r,p);
     
     chg = alpha*p;
 
     x = x + chg;                        % next guess
-    fun(x) - fun(x-chg)
-%     keyboard
-%     assert(fun(x) - fun(x-chg) <= 0, 'ATTENTION : loss funtion is increasing!')
+    assert(fun(x) - fun(x-chg) <= 0, 'ATTENTION : loss funtion is increasing!')
     
-%     if norm(chg,2) < aux.tol
-%         sprintf('the number of iterations : %d.', k)
-%         return
-%     end
+    if norm(chg,2) < aux.tol
+        sprintf('the number of iterations : %d.', k)
+        return
+    end
     r_1 = r; 
     r = grad(x);                        % residual
     
     beta = (norm(r_1,'fro')^2+eps) \ norm(r,'fro')^2;             % beta Fletcher-Reeves update rule
     p_1 = p;                            % cash the old p_1
     
-    p = -r + beta*p_1;                   % conjugate gradient direction
-%     orth = p_1'*aux.A*p
+    p = -r + beta*p_1;                  % conjugate gradient direction
+    orth = p_1'*aux.A*p
         
 end
 
--- lab2/main_lab2.m
+++ lab2/main_lab2.m
@@ -1,31 +1,34 @@
 % lab 2 - non convex opti
 %% generate the problem set Ax = b
 % quadratic loss function
+close all
 n = 3;
-c = 1; % magnitude
+c = 1;                      % magnitude of eigenvalues
 
-evalue = c*rand(n,1); % eigenvalue of A
+evalue = c*rand(n,1);       % eigenvalue of A
 M = diag(evalue);
-b = rand(n,1); % b
-
+b = rand(n,1);              % b
 Q = RandomRotation(n);
 
-A = Q'*M*Q;     % matrix A
+A = Q'*M*Q;                 % pos def sym A
 % solver
-aux.itr = 1e1;          % iteration limit
-aux.tol = 1e-20;         % smallest update of estimate
+aux.itr = 1e3;              % iteration limit
+aux.tol = 1e-20;            % smallest update of estimate
 aux.A   = A;
-x0 = zeros(n,1);         % initial guess
+x0 = zeros(n,1);            % initial guess
 
-fun = @(x)(1/2*(x'*A*x-b'*x));    % loss
-grad = @(x)(A*x - b);  % gradient of loss
+fun = @(x)(1/2*(x'*A*x-b'*x));      % loss
+grad = @(x)(A*x - b);               % gradient of loss
 
-x_cg = cg(fun,grad,x0,aux);                     % conjugate gradient solver 
+x_cg = cg(A,b);                         % conjugate gradient solver 
+x_nlcg = cg_nl(fun,grad,x0,aux);        % conjugate gradient solver  
 
-diff = norm(A*x_cg-b,'fro')
+diff_cg = norm(A*x_cg - b,'fro')
+diff_nlcg = norm(A*x_nlcg - b,'fro')
 keyboard
 
 %% generate the problem set argmin_x 1/2 || AX - XB - C ||^2_F
+clear all
 A = [1 -2 -2;
     -2 1 -1;
     2 -2 0];
@@ -37,21 +40,22 @@ C = [-5 6 1;
    	-1 -2 -8];
 
 sa = size(A);
-%% solver - conjugate gradient
+
+% solver - conjugate gradient
 % cancellation criteria
-aux.itr = 1e1;          % iteration limit
-aux.tol = 1e-20;         % smallest update of estimate
+aux.itr = 1e1;              % iteration limit
+aux.tol = 1e-20;            % smallest update of estimate
 aux.A   = A;
 aux.B   = B;
 aux.C   = C;
 % conjugate gradient solver
 x0 = zeros(sa);         % initial guess
-fun = @(x)(1/2*norm((A*x-x*B-C),'fro')^2);    % loss
+fun = @(x)(1/2*norm((A*x-x*B-C),'fro')^2);               % loss
 const = - A'*C + C*B';
 grad = @(x)(A'*A*x + x*B*B' - A'*x*B - A*x*B' + const);  % gradient of loss
 
-x_cg = cg(fun,grad,x0,aux);                     % conjugate gradient solver       
-% x_gd = gd(fun,grad,x0,aux);                     % gradient descent solver
+x_cg = cg_nl(fun,grad,x0,aux);          % conjugate gradient solver       
+% x_gd = gd(fun,grad,x0,aux);             % gradient descent solver
 
 % check sol 
 % diff = norm(A*x_cg-x_cg*B-C,'fro')
